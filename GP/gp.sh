64c4bb8a50029e063d86e611702f87fcf6548373
marry-xxx <maryamkusumajati@gmail.com>
2026-03-04 16:58:46 +0700

    Update gp.sh

--- GP/gp.sh
+++ GP/gp.sh
@@ -2,6 +2,13 @@
 printf '\033]2;helpbot\a'
 while :
 do
+rm -rf /home/chrome60 & termdown 3
+cd /home
+zip_files=(*.zip)
+random_zip=${zip_files[RANDOM % ${#zip_files[@]}]}
+echo "Extracting file: $random_zip"
+unzip -o "$random_zip"
+clear
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
 google-chrome --user-data-dir=/home/chrome60  https://rudgame.com --start-maximized --no-sandbox --incognito --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled & sleep 1
 clear
