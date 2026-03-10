74fe34cff8595cfe8bf9bb15dde9286ef9a06c38
marry-xxx <maryamkusumajati@gmail.com>
2026-03-10 13:04:49 +0700

    Update gp.sh

--- GP/gp.sh
+++ GP/gp.sh
@@ -2,21 +2,19 @@
 printf '\033]2;helpbot\a'
 while :
 do
-rm -rf /home/chrome60 & termdown 3
-cd /home
-zip_files=(*.zip)
-random_zip=${zip_files[RANDOM % ${#zip_files[@]}]}
-echo "Extracting file: $random_zip"
-unzip -o "$random_zip"
+rm -rf /home/chrome60/Default/Cache/*
+rm -rf /root/Downloads/*
+sleep 2
+ google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60  https://rudgame.com --start-maximized --no-sandbox --incognito --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled & sleep 5
 clear
- google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
- google-chrome --user-data-dir=/home/chrome60  https://rudgame.com --start-maximized --no-sandbox --incognito --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled & sleep 1
+google-chrome --user-data-dir=/home/chrome60  https://rudgame.com --start-maximized --no-sandbox --incognito --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled & sleep 3
 clear
 sleep 10
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/gp.ascr
-sleep 30
+sleep 5
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
